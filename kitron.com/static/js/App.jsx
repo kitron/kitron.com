bc73c68a9308f83dbd4a686cdf68678d31e0aca9
Dan Bebenita <kitron@gmail.com>
2018-08-01 23:22:46 -0700

    some changes

--- kitron.com/static/js/App.jsx
+++ kitron.com/static/js/App.jsx
@@ -3,6 +3,10 @@ import React from "react";
 
 export default class App extends React.Component {
   render () {
-    return <p> Hello React!</p>;
+  return (
+      <p>
+      <br/><center><h1>ALEXIS.COM</h1></center>
+      </p>
+    );
   }
 }
